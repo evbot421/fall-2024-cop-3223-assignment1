72e18d7f981415a77492f7c53b716989c2d55150
evbot421 <ev552461@ucf.edu>
2024-09-08 23:02:57 +0000

    finished my code!

--- fracturing.c
+++ fracturing.c
@@ -1,7 +1,18 @@
+
+
+
+
+
+
+
+
+
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
 
+double askForUserInput();
+double DistanceFormula();
 double calculateDistance();
 double calculatePerimeter();
 double calculateArea();
@@ -19,26 +30,48 @@ int main(int argc, char** arg){
     return 0;
 }
 
+double askForUserInput(){
+    double value = 0.0;
+    printf("please enter your value\n");
+    scanf("%lf",&value);
+    return value;
+}
+
+double DistanceFormula(){
+
+
+    double x1 = askForUserInput();
+    double x2= askForUserInput();
+    double y1= askForUserInput();
+    double y2= askForUserInput();
+
+
+
+
+    double Distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("Point #1 Entered: x1 = %.3f ; y1 = %.3f \n", x1, y1);
+    printf("Point #2 entered: x2 = %.3f ; y2 = %.3f \n", x2, y2);
+
+    return Distance;
+
+
+}
+
 
 
 double calculateDistance (){
    
-    double x1 = 0.0; 
-    double y1 = 0.0;
-    double x2 = 0.0;
-    double y2 = 0.0;
+    double x1=0.0;
+    double x2=0.0;
+    double y1=0.0;
+    double y2=0.0;
 
 
-    printf("please enter your x values\n");
-    scanf("%lf %lf", &x1, &x2);
-    printf("please enter your y values\n");
-    scanf("%lf %lf", &y1, &y2);
 
-    double Distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double Distance = DistanceFormula();
 
-    printf("Point #1 Entered: x1 = %lf ; y1 = %lf \n", x1, y1);
-    printf("Point #2 entered: x2 = %lf ; y2 = %lf \n", x2, y2);
-    printf("The distance between the two points is %lf\n", Distance);
+
+    printf("The distance between the two points is %.3f\n", Distance);
 
 
     return Distance;}
@@ -54,15 +87,14 @@ double calculatePerimeter(){
     double x2 = 0.0;
     double y2 = 0.0;
   
+   
 
-    double Diameter = calculateDistance();
+    double Diameter = DistanceFormula();
 
     double Perimeter = PI * Diameter;
 
-    printf("Point #1 Entered: x1 = %lf ; y1 = %lf \n", x1, y1);
-    printf("Point #2 entered: x2 = %lf ; y2 = %lf \n", x2, y2);
 
-    printf("The perimeter of the city encompassed by your request is %lf \n", Perimeter);
+    printf("The perimeter of the city encompassed by your request is %.3f \n", Perimeter);
 
     double difficulty2 = 3.6489;
     return difficulty2;
@@ -77,16 +109,16 @@ double calculateArea(){
     double x2 = 0.0;
     double y2 = 0.0;
 
+ 
 
-    double Diameter = calculateDistance();
+    double Diameter = DistanceFormula();
     double Radius = Diameter/2;
 
     double Area = PI * pow(Radius, 2);
 
-    printf("Point #1 Entered: x1 = %lf ; y1 = %lf \n", x1, y1);
-    printf("Point #2 entered: x2 = %lf ; y2 = %lf \n", x2, y2);
 
-    printf("The area of the city encompassed by your request is %lf \n", Area);
+
+    printf("The area of the city encompassed by your request is %.3f \n", Area);
 
     double difficulty3 = 3.201;
 
@@ -100,14 +132,13 @@ double calculateWidth(){
     double x2 = 0.0;
     double y2 = 0.0;
 
+  
 
-    double Diameter = calculateDistance();
+    double Diameter = DistanceFormula();
     double Width = Diameter;
 
-    printf("Point #1 Entered: x1 = %lf ; y1 = %lf \n", x1, y1);
-    printf("Point #2 entered: x2 = %lf ; y2 = %lf \n", x2, y2);
-
-    printf("The width of the city encompassed by your request is %lf\n ", Width);
+  
+    printf("The width of the city encompassed by your request is %.3f\n ", Width);
 
     double difficulty4 = 2.205;
 
@@ -121,20 +152,31 @@ double calculateHeight(){
     double x2 = 0.0;
     double y2 = 0.0;
 
+  
 
-    double Diameter = calculateDistance();
+    double Diameter = DistanceFormula();
     double Height = Diameter;
 
-    printf("Point #1 Entered: x1 = %lf ; y1 = %lf \n", x1, y1);
-    printf("Point #2 entered: x2 = %lf ; y2 = %lf \n", x2, y2);
+    
 
-    printf("The height of the city encompassed by your request is %lf\n ", Height);
+    printf("The height of the city encompassed by your request is %.3f\n ", Height);
 
     double difficulty5 = 1.224;
 
     return difficulty5;
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
